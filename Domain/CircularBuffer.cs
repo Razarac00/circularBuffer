50a4af7ce7f9555ce7b7393f7ebd2e74eed0159f
Razarac00 <orji.john@outlook.com>
2020-09-15 10:28:00 -0500

    some comments on enumeration

---
+++ Domain/CircularBuffer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Domain
+{
+    public class CircularBuffer
+    {
+        public CircularBuffer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CircularBuffer(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddtoBuffer(char x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char ReadfromBuffer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable GetIterator() // to implement foreach, we need a public definition of GetEnumerator
+        {
+            throw new System.NotImplementedException();
+        }
+        // We need a class that implements the IEnumerator interface, which means implementing IEnumerator.Current, MoveNext(), Reset()
+    }
+}
